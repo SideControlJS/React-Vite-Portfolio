83f1aa1dc86b2715172c670a9dcb260f84bd1375
Jesse <jesse.lare@gmail.com>
2023-12-03 23:37:41 -0500

    refactoring AboutMe.jsx

--- src/components/3. Footer Components/AboutMe/AboutMe.jsx
+++ src/components/3. Footer Components/AboutMe/AboutMe.jsx
@@ -3,30 +3,36 @@ import './AboutMe.css';
 import ContactMe from '../ContactMe/ContactMe';
 import { aboutMeText } from '../../../myInfo';
 
+// created two subcomponents: AboutMeInfo and AboutMeContact
+const AboutMeInfo = ({ infotext, power_slogan }) => (
+  <div className="cta-content">
+    <h2 className="wow fadeInUp" data-wow-delay=".2s">About me:</h2>
+    <p className="wow fadeInUp" data-wow-delay=".4s">
+      <hr /> {infotext} <hr />
+      <h2>{power_slogan}</h2>
+    </p>
+  </div>
+);
+
+const AboutMeContact = () => (
+  <div className="contact-sidebar">
+    <ContactMe />
+  </div>
+);
+
+// added AboutMe subcomponent
 const AboutMe = () => {
-  const { infotext , power_slogan } = aboutMeText;
+  const { infotext, power_slogan } = aboutMeText;
+
   return (
     <section id="AboutMe" className="section About">
       <div className="container">
         <div className="row">
           <div className="col-lg-6 col-md-12 d-flex align-items-center">
-            <div className="cta-content">
-              <h2 className="wow fadeInUp" data-wow-delay=".2s">
-                About me: <br />
-              </h2>
-
-              <p className="wow fadeInUp" data-wow-delay=".4s">
-                <hr /> {infotext}
-              
-                <hr />
-                <h2>{power_slogan} </h2>
-              </p>
-            </div>
+            <AboutMeInfo infotext={infotext} power_slogan={power_slogan} />
           </div>
           <div className="col-lg-6 col-md-12">
-            <div className="contact-sidebar">
-              <ContactMe />
-            </div>
+            <AboutMeContact />
           </div>
         </div>
       </div>
